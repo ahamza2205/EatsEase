39e5158314dc99e133f5f25c0f084dd7fd9c4d7d
Abdul-Rahman Hamza <abdulrahmanhamza2205@gmail.com>
2024-08-14 14:30:21 +0300

    login and sign up UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.gms)
+    alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
@@ -37,9 +39,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.auth)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation (libs.rxjava) // Replace with desired version
+
 
 }
--- app/src/main/java/com/example/eatsease/login_signup/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/eatsease/login_signup/ui/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.eatsease;
+package com.example.eatsease.login_signup.ui.activity;
 
 import android.os.Bundle;
 
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.eatsease.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
